6bea8be3992e4a9c345700d806ed80bce65c5ef1
fpatel02 <patelfauram1@outlook.com>
2024-12-10 13:25:06 -0600

    created users.csv

--- users.py
+++ users.py
@@ -12,7 +12,7 @@ def load_users(file = 'users.csv'):
     return users
 
 # Add a new user
-def add_user(email, file = 'subscribers.csv'):
+def add_user(email, file = 'users.csv'):
     with open(file, mode = 'a', newline = '') as file:
         writer = csv.writer(file)
         writer.writerow([email])
