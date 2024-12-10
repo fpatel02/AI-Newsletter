7012aac39415c48daa2e3ab12c5cf3e58d8e4b20
fpatel02 <patelfauram1@outlook.com>
2024-12-10 13:19:19 -0600

    added code for email_sender

--- email_sender.py
+++ email_sender.py
@@ -1,1 +1,27 @@
+# email_sender.py  
+
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_newsletter(subject, body, recipients):
+    sender_email = "[email]@gmail.com"
+    sender_password = "[password]"
+
+    # email server setup 
+    server = smtplib.SMTP ("smtp.gmail.com", 587) # 587 is default SMTP port
+    server.starttls()
+    server.login(sender_email, sender_password)
+
+    # email message setup
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "html"))
+
+    # send email
+    for recipient in recipients:
+        msg["To"] = recipient
+        server.sendmail(sender_email, recipient, msg.as_string())
+    
+    server.quit()
