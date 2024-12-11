a9c1417f35d67c8937a040841725e1dd0f62a67f
fpatel02 <patelfauram1@outlook.com>
2024-12-10 18:32:32 -0600

    updated code in email_sender to reference .env variable

--- email_sender.py
+++ email_sender.py
@@ -3,10 +3,14 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from dotenv import load_dotenv, dotenv_values
+import os
+
+load_dotenv
 
 def send_newsletter(subject, body, recipients):
-    sender_email = "[email]@gmail.com"
-    sender_password = "[password]"
+    sender_email = os.getenv("EMAIL")
+    sender_password = os.getenv("PASSWORD")
 
     # email server setup 
     server = smtplib.SMTP ("smtp.gmail.com", 587) # 587 is default SMTP port
